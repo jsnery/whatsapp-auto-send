7ee3cde68fe8ded99daee47b483c89effc1acc72
Richard Neri <richardmatq@gmail.com>
2023-01-12 17:43:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import webbrowser
-import pyautogui
+from pyautogui import hotkey
 from time import sleep
 
 
-def tab(x):
+def hotkey_tab(x):
     for i in range(x):
-        pyautogui.hotkey('tab')
+        hotkey('tab')
 
 
 contato = 5579999068342
 msg = f"https://api.whatsapp.com/send?phone={contato}&text=Sua%20assinatura%20Expirou%20%E2%9A%A0%EF%B8%8F%0A%0A%F0%9F%92%B2%20Mensal:%20R$35,00%0A%F0%9F%92%B2%20Bimensal:%20R$60,00%0A%F0%9F%92%B2%20Trimestral:%20R$85,00%0A%F0%9F%92%B2%20Semestral:%20R$160,00%0A%F0%9F%92%B2%20Anual:%20R$300,00%0A%0A%F0%9F%92%B5%20Pix:%2008323217530"
 
-webbrowser.open_new_tab(msg)
+webbrowser.open_new(msg)
 sleep(2)
-tab(8)
-pyautogui.hotkey('enter')
+hotkey_tab(8)
+hotkey('enter')
 sleep(2)
-tab(2)
-pyautogui.hotkey('enter')
+hotkey_tab(2)
+hotkey('enter')
 sleep(15)
-pyautogui.hotkey('enter')
+hotkey('enter')
 sleep(1)
-pyautogui.hotkey('a
-', 'f4')
+hotkey('ctrl', 'w')
